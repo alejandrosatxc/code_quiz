ee1dc1fc7786cac5c581f4fa3ac6bf73feb1bc10
Alejandro Zapien <alejandro@satxconsultants.com>
2022-11-05 14:54:17 -0500

    Comments

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-var quizTimer = null
-var time_remaining = 60
-var score = 0
-var highscores = []
-var mainComponents = $('#main-container').children()
-mainComponents.hide()
-$('#welcome').show()
+var quizTimer = null //Variable to keep track of the game timer
+var time_remaining = 60 //Time Remaining for the game
+var score = 0 //User's score
+var highscores = [] //Array to hold highscore objects for reading and writing to localStorage
+var mainComponents = $('#main-container').children() //All distinct components in <main>
+mainComponents.hide() //Hide all components first
+$('#welcome').show() //First show welcome component
+//Add Event handlers to all buttons in the quiz
 $('.home-btn').on('click', home)
 $('.start-btn').on('click', startQuiz)
 $('.reset-highscores-btn').on('click', resetHighscores)
@@ -13,8 +14,11 @@ $('#submit-btn').on('click', submitHighScore)
 
 loadHighscores()
 
+//Starts a countdown timer for the quiz
 function countdown() {
+    //start at 60 seconds
     time_remaining = 60
+    //Update time-remaining element
     $('#time-remaining').text("Time Remaining: " + time_remaining)
     time_remaining--
     quizTimer = setInterval(function () {
@@ -32,9 +36,11 @@ function startQuiz() {
     //Start a timer
     countdown()
 
+    //Hide all components, then show the timer and question components
     $('#main-container').children().hide()
     $('#time-remaining').show()
     $('#questions').show()
+    //An Array of objects describing questions, their answer choices and the correct answer
     var slides = [
         {
             question: "Which of the following is a programming language? Like an actual programming language",
@@ -62,11 +68,15 @@ function startQuiz() {
             correct_answer: "All of it"
         }
     ]
+    //Use 'i' to track what question we are on
     var i = 0;
+    //Get total # of questions in array
     var total_questions = slides.length
+    //Remove one question object and present to the user
     var slide = slides.pop()
 
-    //present question
+    //present question by injecting question and answer choices into the question component
+    //from the array of questions
     $('.card-title').text(`Question ${i + 1} / ${total_questions}`)
     $('.card-text').text(slide.question)
     $('#a').text(slide.answer_choices[0])
@@ -79,11 +89,17 @@ function startQuiz() {
     $('.alert-danger').hide()
     //Add Click handler, use delegation to get users choice
     $('#answer-choices').on('click', function (e) {
+        //increment i by 1 to track which question we are on
         i++
+        //Check if the correct answer was clicked
         if ($(e.target).text() === slide.correct_answer) {
+            //If it was, add to score the number of points that question is worth
+            //out of the total number of questions
             score += 100 / total_questions
+            //Give positive feedback
             $('.alert-success').show()
         } else {
+            //If wrong, give negative feedback and deduct 5 seconds from total time
             $('.alert-danger').show()
             time_remaining -= 5
             $('#time-remaining').text("Time Remaining: " + time_remaining)
@@ -94,12 +110,14 @@ function startQuiz() {
         } else {
             //check if there are still questions,
             if (slides.length === 0) {
+                //if no more questions end quiz
                 setTimeout(function () {
                     $('.alert-success').hide()
                     $('.alert-danger').hide()
                     endQuiz()
                 },1000)
             } else {
+                //Otherwise, after a one second delay, show the next question
                 setTimeout(function () {
                     $('.alert-success').hide()
                     $('.alert-danger').hide()
@@ -125,6 +143,7 @@ function endQuiz() {
     score += time_remaining
     //Hide all components
     mainComponents.hide()
+    //Show the done component with the users score
     $('#done').show()
     $('#score').text(score.toFixed(2))
 }
@@ -147,12 +166,14 @@ function submitHighScore() {
     highscores.push(highscore)
     //Stringify that array, and save it to local storage
     localStorage.setItem('codingQuizHighScores', JSON.stringify(highscores))
-    
+
     showHighscores()
 }
 
 function showHighscores() {
+    //Hide all components, 
     mainComponents.hide()
+    //Show the highscore component
     loadHighscores()
     $('#highscores').show()
 }
@@ -164,10 +185,15 @@ function loadHighscores() {
 
     //Load high scores from local storage
     var data = localStorage.getItem('codingQuizHighScores')
+    //If previous highscores were found,
     if (data) {
+        //parse the string representation of the array containing highscore objects
         highscores = JSON.parse(data)
+        //Sort the array of objects from least highscore to greatest highscore
         highscores.sort((a, b) => b.score - a.score)
+        //For each highscore object in the array of highscores
         for (var i in highscores) {
+            //Append a row of data to the table in the highscore component
             $('tbody').append(
                 '<tr>' +
                 '<th scope="row">' + i + '</th>' +
@@ -177,10 +203,12 @@ function loadHighscores() {
             )
         }
     } else {
+        //Otherwise, no previous highscores were found, set the array oh highscores to an empty highscore
         highscores = []
     }
 }
 
+//Clear any existing highscores from localStorage, the table in the highscores component, and the array of highscores
 function resetHighscores() {
     localStorage.clear()
     $('tbody').children().remove()
